5440f95108eb312c1313a244aa360a8db188d21b
Your Name <you@example.com>
2026-04-11 11:42:03 -0400

    addded basic pinouts

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Analog soil sensor pins (A1 - A4)
+const int SOIL_SENSOR_1 = 1;
+const int SOIL_SENSOR_2 = 2;
+const int SOIL_SENSOR_3 = 3;
+const int SOIL_SENSOR_4 = 4;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+  Serial.println("Soil sensor reader starting");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int v1 = analogRead(SOIL_SENSOR_1);
+  int v2 = analogRead(SOIL_SENSOR_2);
+  int v3 = analogRead(SOIL_SENSOR_3);
+  int v4 = analogRead(SOIL_SENSOR_4);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Sensor 1 (A1): "); Serial.println(v1);
+  Serial.print("Sensor 2 (A2): "); Serial.println(v2);
+  Serial.print("Sensor 3 (A3): "); Serial.println(v3);
+  Serial.print("Sensor 4 (A4): "); Serial.println(v4);
+  Serial.println("---");
+
+  delay(1000);
 }
