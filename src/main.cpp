4d6928a5e79b3714f8e5e522a92cb8a72913301d
Your Name <you@example.com>
2026-04-11 13:12:33 -0400

    added stuff working on sorting out json stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "your_SSID";
 const char* password = "your_PASSWORD";
+
+// webserver on port 80
+WebServer server(80);
 
 // Analog soil sensor pins (A1 - A4)
 const int SOIL_SENSOR_1 = 1;
@@ -29,13 +33,38 @@ void hardwareSetup() {
 }
 
 void readSoilMoisture() {
+  Serial.println("Reading soil moisture levels...");
   int moisture1 = analogRead(SOIL_SENSOR_1);
   int moisture2 = analogRead(SOIL_SENSOR_2);
   int moisture3 = analogRead(SOIL_SENSOR_3);
   int moisture4 = analogRead(SOIL_SENSOR_4);
-
-
+Serial.println("Red soil moisture levels...");
 }
+
+// Base json object to hold all the data
+
+void dataJson(int sensor_name, int sensor_data_raw,int sensor_data_avg, bool valve_state) {
+   StaticJsonDocument<200> doc;
+
+  doc["timestamp"] = millis();
+  doc["sensor"] = sensor_name;
+  doc["moisture_raw"] = sensor_data_raw;
+  doc["moisture_avg"] = sensor_data_avg;
+  doc["valve_state"] = valve_state;
+
+  String output;
+  serializeJson(doc, output);
+
+  server.send(200, "application/json", output);
+}
+/*
+Endpoints for the web server will be defined here, such as handling requests to read soil moisture levels or control the water pump.
+*/
+
+
+
+void handleSensorData() { dataJson(1,1,1,1); }
+
 
 void setup() {
 hardwareSetup();
