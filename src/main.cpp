a9261cd24ecf40291ab6a3fb29b15a6bf44ae1e9
Nicky Blackburn <Nickkyblackburn@gmail.com>
2026-04-19 12:23:40 -0400

    working on adding stuff ngl

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,46 @@
-#include <Arduino.h>
 #include <WiFi.h>
-#include <WebServer.h>
-#include <ArduinoJson.h>
+#include <HTTPClient.h>
 
 const char* ssid = "your_SSID";
 const char* password = "your_PASSWORD";
 
-// webserver on port 80
-WebServer server(80);
-
-// Analog soil sensor pins (A1 - A4)
-const int SOIL_SENSOR_1 = 1;
-const int SOIL_SENSOR_2 = 2;
-const int SOIL_SENSOR_3 = 3;
-const int SOIL_SENSOR_4 = 4;
-
- // Digital pin for water pump control
-const int WATER_PUMP_PIN = 5;
-
-/*
-Setups the hardware components, such as initializing the soil sensor pins as input.
-*/
-void hardwareSetup() {
-  // Initialize soil sensor pins as input
-  pinMode(SOIL_SENSOR_1, INPUT);
-  pinMode(SOIL_SENSOR_2, INPUT);
-  pinMode(SOIL_SENSOR_3, INPUT);
-  pinMode(SOIL_SENSOR_4, INPUT);
-
-  // Initialize water pump control pin as output
-  pinMode(WATER_PUMP_PIN, OUTPUT);
-}
-
-void readSoilMoisture() {
-  Serial.println("Reading soil moisture levels...");
-  int moisture1 = analogRead(SOIL_SENSOR_1);
-  int moisture2 = analogRead(SOIL_SENSOR_2);
-  int moisture3 = analogRead(SOIL_SENSOR_3);
-  int moisture4 = analogRead(SOIL_SENSOR_4);
-Serial.println("Red soil moisture levels...");
-}
-
-// Base json object to hold all the data
-
-void dataJson(int sensor_name, int sensor_data_raw,int sensor_data_avg, bool valve_state) {
-   StaticJsonDocument<200> doc;
-
-  doc["timestamp"] = millis();
-  doc["sensor"] = sensor_name;
-  doc["moisture_raw"] = sensor_data_raw;
-  doc["moisture_avg"] = sensor_data_avg;
-  doc["valve_state"] = valve_state;
-
-  String output;
-  serializeJson(doc, output);
-
-  server.send(200, "application/json", output);
-}
-/*
-Endpoints for the web server will be defined here, such as handling requests to read soil moisture levels or control the water pump.
-*/
-
-
-
-void handleSensorData() { dataJson(1,1,1,1); }
-
+const int sensorPins[] = {34, 35, 32, 33}; // REAL analog pins
 
 void setup() {
-hardwareSetup();
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 }
 
 void loop() {
+  int total = 0;
 
+  for (int i = 0; i < 4; i++) {
+    total += analogRead(sensorPins[i]);
+  }
 
-  delay(1000);
-}
+  float avg = total / 4.0;
+
+  HTTPClient http;
+  http.begin("http://YOUR_SERVER_IP:8000/api/bed-data");
+  http.addHeader("Content-Type", "application/json");
+
+  String json = "{";
+  json += "\"bed_id\":\"bed_1\",";
+  json += "\"average\":" + String(avg) + ",";
+  json += "\"sensors\":[";
+
+  for (int i = 0; i < 4; i++) {
+    json += String(analogRead(sensorPins[i]));
+    if (i < 3) json += ",";
+  }
+
+  json += "],\"valve_state\":\"OFF\"}";
+
+  http.POST(json);
+  http.end();
+
+  delay(3000);
